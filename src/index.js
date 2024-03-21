0849162cffe8da0026db0e2a288b4ec0adf1b9cc
joonhyung park <joonharry706@gmail.com>
2024-03-21 23:33:15 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AppClass from "./AppClass";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppClass />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
